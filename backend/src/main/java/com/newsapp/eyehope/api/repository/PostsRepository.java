2705c4f2463c53f1773501552564b811fdb8702f
jhyeon1027 <jhyeon1027@naver.com>
2025-08-27 13:26:36 +0900

    feat: Repository 클래스 추가

--- backend/src/main/java/com/newsapp/eyehope/api/repository/PostsRepository.java
+++ backend/src/main/java/com/newsapp/eyehope/api/repository/PostsRepository.java
@@ -1,4 +1,15 @@
 package com.newsapp.eyehope.api.repository;
 
-public class PostsRepository {
+import com.newsapp.eyehope.api.domain.Posts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+    Optional<Posts> findByUrl(String link); // 중복 체크용
+    boolean existsByUrl(String link);
 }
