cedf38057c4975c19f029dd39bc722b3f3591607
jhyeon1027 <jhyeon1027@naver.com>
2025-08-27 13:25:50 +0900

    feat: DTO 클래스 추가

--- backend/src/main/java/com/newsapp/eyehope/api/dto/PostRequestDto.java
+++ backend/src/main/java/com/newsapp/eyehope/api/dto/PostRequestDto.java
@@ -1,4 +1,16 @@
 package com.newsapp.eyehope.api.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class PostRequestDto {
+    private String title;
+    private String content;
+    private String source;
+    private String url;
+    private String created_at;
 }
