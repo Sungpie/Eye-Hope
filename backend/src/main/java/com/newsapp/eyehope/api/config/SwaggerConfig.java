aa7d7cb4413fce1578b6a41945ccbead37894f81
jhyeon1027 <jhyeon1027@naver.com>
2025-09-08 22:03:03 +0900

    feat: Swagger UI HTTPS 설정

--- backend/src/main/java/com/newsapp/eyehope/api/config/SwaggerConfig.java
+++ backend/src/main/java/com/newsapp/eyehope/api/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +13,17 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI newsAppOpenAPI() {
+        Server httpsServer = new Server();
+        httpsServer.setUrl("https://eyehope.site");
+        httpsServer.setDescription("HTTPS Server");
+
+        Server httpLocalServer = new Server();
+        httpLocalServer.setUrl("http://localhost:8080");
+        httpLocalServer.setDescription("Local HTTP Server");
+
         return new OpenAPI()
+                .addServersItem(httpsServer)
+                .addServersItem(httpLocalServer)
                 .info(new Info()
                         .title("뉴스 요약 앱 API")
                         .description("뉴스앱 백엔드 API 문서")
@@ -22,6 +33,6 @@ public class SwaggerConfig {
                                 .url("https://github.com/Sungpie/Eye-Hope"))
                         .license(new License()
                                 .name("Apache 2.0")
-                                .url("http://springdoc.org")));
+                                .url("https://springdoc.org")));
     }
 }
